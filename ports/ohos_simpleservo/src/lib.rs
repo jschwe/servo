bf95905598b2f4238475d6f5e42ea267d309bc86
Jonathan Schwender <schwenderjonathan@gmail.com>
2024-03-01 17:00:49 +0100

    Add an app skeleton for OpenHarmony.
    This skeleton allows verifying that `libservo` builds for OpenHarmony.
    
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

---
+++ ports/ohos_simpleservo/src/lib.rs
@@ -0,0 +1,5 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+mod gl_glue;
+// Empty dummy library - to be added in a future commit.

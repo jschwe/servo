f86a3c1c6b6b3abcbdebb5b7b14827684082df40
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-05-03 11:56:28 +0200

    Fix rebase issue in font_list.rs
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

--- components/gfx/platform/freetype/ohos/font_list.rs
+++ components/gfx/platform/freetype/ohos/font_list.rs
@@ -153,11 +153,7 @@ where
             },
             None => StyleFontStyle::NORMAL,
         };
-        let descriptor = FontTemplateDescriptor {
-            weight,
-            stretch,
-            style,
-        };
+        let descriptor = FontTemplateDescriptor::new(weight, stretch, style);
         callback(FontTemplate::new_local(local_font_identifier, descriptor));
     };
 

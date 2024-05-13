bfa9d149894cca5a4f7f98f9d9eed011ed46ebe3
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-05-13 14:20:07 +0200

    gfx: font_list: Rebase changes
    Adapt changes from 1c9120c293cc16c11637feb6003117d4093642b5

--- components/gfx/platform/freetype/ohos/font_list.rs
+++ components/gfx/platform/freetype/ohos/font_list.rs
@@ -4,16 +4,16 @@
 
 use std::fs::File;
 use std::io::Read;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use log::warn;
+use malloc_size_of_derive::MallocSizeOf;
 use serde::{Deserialize, Serialize};
 use style::values::computed::{
     FontStretch as StyleFontStretch, FontStyle as StyleFontStyle, FontWeight as StyleFontWeight,
 };
 use style::Atom;
 use ucd::{Codepoint, UnicodeBlock};
-use webrender_api::NativeFontHandle;
 
 use crate::font_template::{FontTemplate, FontTemplateDescriptor};
 use crate::text::util::is_cjk;
@@ -24,7 +24,7 @@ lazy_static::lazy_static! {
 }
 
 /// An identifier for a local font on OpenHarmony systems.
-#[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, Hash, MallocSizeOf, PartialEq, Serialize)]
 pub struct LocalFontIdentifier {
     /// The path to the font.
     pub path: Atom,
@@ -154,11 +154,10 @@ where
             },
             None => StyleFontStyle::NORMAL,
         };
-        let descriptor = FontTemplateDescriptor {
-            weight,
+        let descriptor = FontTemplateDescriptor::new(weight,
             stretch,
             style,
-        };
+        );
         callback(FontTemplate::new_for_local_font(
             local_font_identifier,
             descriptor,

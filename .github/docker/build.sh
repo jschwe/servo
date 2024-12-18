cb40431cb183446467ec5b16a68acf44d40a1038
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-12-18 13:47:13 +0000

    try docker in linux job

--- .github/docker/build.sh
+++ .github/docker/build.sh
@@ -8,3 +8,10 @@ docker build \
  --target servo_cooked_dev-crown-default_features \
  --tag ghcr.io/jschwe/servo_ci_testing_dev-crown-default_features:latest \
  .
+
+docker build \
+ --label "org.opencontainers.image.source=https://github.com/servo/servo" \
+ --label "org.opencontainers.image.description=Docker image for servo CI" \
+ --target servo_cooked_release-crown-default_features \
+ --tag ghcr.io/jschwe/servo_ci_testing_release-crown-default_features:latest \
+ .

75207a138747580af7ac5c7018424dc4e70c55d5
Jonathan Schwender <schwenderjonathan@gmail.com>
2024-12-19 09:37:03 +0100

    Remove docker user
    Github mounts $GITHUB_ENV as a bindmount, meaning we need to
    run as the root user in the container.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- .github/docker/build.sh
+++ .github/docker/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null || exit 1
 
-docker build \
- --label "org.opencontainers.image.source=https://github.com/servo/servo" \
- --label "org.opencontainers.image.description=Docker image for servo CI" \
- --target servo_cooked_dev-crown-default_features \
- --tag ghcr.io/jschwe/servo_ci_testing_dev-crown-default_features:latest \
- .
+#docker build \
+# --label "org.opencontainers.image.source=https://github.com/servo/servo" \
+# --label "org.opencontainers.image.description=Docker image for servo CI" \
+# --target servo_cooked_dev-crown-default_features \
+# --tag ghcr.io/jschwe/servo_ci_testing_dev-crown-default_features:latest \
+# .
 
 docker build \
  --label "org.opencontainers.image.source=https://github.com/servo/servo" \
